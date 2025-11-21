e46c8abf97d32df6f2f81748a4139760d2c1b72e
nosra <77589065+nosra@users.noreply.github.com>
2025-11-21 10:41:13 -0500

    added querying db in jinja for plugins, dummy plugins for MVP

--- mpc_database/home/views.py
+++ mpc_database/home/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
+from .models import ProPlugin, AlternativePlugin
 
 def home(request):
     template = loader.get_template('home.html')
     return HttpResponse(template.render())
 
 def plugins(request):
-    template = loader.get_template('plugins.html')
-    return HttpResponse(template.render())
+    pro_plugins = ProPlugin.objects.all()
+    alt_plugins = AlternativePlugin.objects.all()
+    return render(request, "plugins.html", {
+        "pro_plugins": pro_plugins,
+        "alt_plugins": alt_plugins,
+    })
+    
+def plugin_detail(request, pk):
+    plugin = get_object_or_404(ProPlugin, pk=pk)
+    return render(request, "plugin_detail.html", {"plugin": plugin})
