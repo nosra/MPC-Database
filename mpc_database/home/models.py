a8c58bc38d1f7de4e0c95dbaca4b3f9cf5271280
nosra <carson7331@gmail.com>
2026-04-15 18:55:14 -0400

    removed local file compression -- cloudinary handles it

--- mpc_database/home/models.py
+++ mpc_database/home/models.py
@@ -141,22 +141,7 @@ class AlternativePlugin(models.Model, RatingMixin):
     def categories(self):
         return Category.objects.filter(subcategories__in=self.subcategories.all()).distinct()
     
-    # overloading the image function to compress images before saving
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-
-        if self.image:
-            img = Image.open(self.image.path)
-
-            if img.height > 800 or img.width > 800:
-                output_size = (800, 800)
-                img.thumbnail(output_size, Image.Resampling.LANCZOS)
-                
-                # overwriting the original large file with the compressed one
-                # quality=85 seems to be a good balance
-                img.save(self.image.path, optimize=True, quality=85)
-
-
+    
 class ProPlugin(models.Model, RatingMixin):
     submitter = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=30)
@@ -190,23 +175,7 @@ class ProPlugin(models.Model, RatingMixin):
     
    # mtm with pro plugin
     alternatives = models.ManyToManyField(AlternativePlugin, related_name="pro_plugins", blank=True)
-
-    # overloading the image function to compress images before saving
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-
-        if self.image:
-            img = Image.open(self.image.path)
-
-            if img.height > 800 or img.width > 800:
-                output_size = (800, 800)
-                img.thumbnail(output_size, Image.Resampling.LANCZOS)
-                
-                # overwriting the original large file with the compressed one
-                # quality=85 seems to be a good balance
-                img.save(self.image.path, optimize=True, quality=85)
-
-
+ 
     def __str__(self):
         return self.name
 
