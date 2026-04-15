dc4226b838d210bae8a5c7a8053851ebb1d86a1d
nosra <carson7331@gmail.com>
2026-04-15 19:11:18 -0400

    cloudinary is automatically appending the file extension, need to strip before saving

--- mpc_database/home/storage.py
+++ mpc_database/home/storage.py
@@ -4,27 +4,23 @@ from django.core.files.storage import Storage
 from django.conf import settings
 import cloudinary
 import logging
+import os
 
 logger = logging.getLogger(__name__)
-
 class CloudinaryStorage(Storage):
     def _save(self, name, content):
-        logger.error(f"CloudinaryStorage._save called with name: {name}")
         cloudinary.config(
             cloud_name=settings.CLOUDINARY_STORAGE['CLOUD_NAME'],
             api_key=settings.CLOUDINARY_STORAGE['API_KEY'],
             api_secret=settings.CLOUDINARY_STORAGE['API_SECRET'],
         )
-        try:
-            result = cloudinary.uploader.upload(content, public_id=name, overwrite=True)
-            logger.error(f"Cloudinary upload result: {result}")
-            return result['public_id']
-        except Exception as e:
-            logger.error(f"Cloudinary upload failed: {e}")
-            raise
-
+        public_id = os.path.splitext(name)[0] # strip extension
+        result = cloudinary.uploader.upload(content, public_id=public_id, overwrite=True)
+        return result['public_id']
+    
     def url(self, name):
-        return cloudinary.CloudinaryImage(name).build_url()
+        public_id = os.path.splitext(name)[0]
+        return cloudinary.CloudinaryImage(public_id).build_url()
 
     def exists(self, name):
         return False
