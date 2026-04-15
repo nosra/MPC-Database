8aa68dd8f1823ac7acc05d8691d595907c894fb9
nosra <carson7331@gmail.com>
2026-04-15 18:49:07 -0400

    deployment debugging

--- mpc_database/mpc_database/settings.py
+++ mpc_database/mpc_database/settings.py
@@ -36,7 +36,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'local-dev-fallback')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+ 
 
 # Application definition
 
